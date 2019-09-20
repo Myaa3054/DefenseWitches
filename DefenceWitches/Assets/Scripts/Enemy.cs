ed5f3a158b981d99eb76607e2c3c2ef30b6fda8b
Myaa3054 <kurikinntonnmannjuu@gmail.com>
2019-09-21 02:30:33 +0900

    初回コミット本の2.7でバグがある助けて　

---
+++ DefenceWitches/Assets/Scripts/Enemy.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// 敵クラス
+public class Enemy : Token
+{
+    //アニメーション用のスプライト 
+    public Sprite spr0;
+    public Sprite spr1;
+
+    // アニメーション タイマー 
+    int tAnim = 0;
+
+    //速度パラメータ 
+    float speed = 0;// 速度 
+    float tSpeed = 0;// 補完値( 0.0〜 100.0)
+
+    //経路座標のリスト
+    List<Vec2D> path;
+
+    //経路の現在の番号 
+    int pathIdx;
+    //チップ座標 
+    Vec2D prev; // 1つ前 
+    Vec2D next; // 1つ先
+
+    //初期化
+    public void Init(List<Vec2D> path)
+    {
+        //経路をコピー 
+        path = path;
+        pathIdx = 0;
+        //移動速度 
+        speed = 2.0f;
+        tSpeed = 0;
+
+        //移動先を更新 
+        MoveNext();
+        //prevに反映 する 
+        prev.Copy(next);
+        //一つ左にずらす 
+        prev.x -= Field.GetChipSize();
+        //一度座標を更新しておく
+        FixedUpdate();
+    }
+
+    // アニメーション更新 
+    void FixedUpdate()
+    {
+        tAnim++;
+        if ( tAnim % 32 < 16)
+        {
+            SetSprite(spr0);
+        }
+        else
+        {
+            SetSprite(spr1);
+        }
+
+        // 速度タイマー更新 
+        tSpeed += speed;
+        if (tSpeed >= 100.0f)
+        {
+            // 移動先を次に進める 
+            tSpeed -= 100.0f;
+            MoveNext();
+        }
+
+        //速度タイマーに対応する位置に線形補間で移動する 
+        X = Mathf.Lerp(prev.x, next.x, tSpeed / 100.0f);
+        Y = Mathf.Lerp(prev.y, next.y, tSpeed / 100.0f);
+    }
+
+    //次の移動先に進める 
+    void MoveNext()
+    {
+        if( pathIdx >= path.Count)
+        {
+            // ゴールにたどりついた 
+            tSpeed = 100.0f;
+            return;
+        }
+        //移動先を移動元にコピーする
+        prev.Copy( next);
+
+        //チップ座標を取り出す 
+        Vec2D v = path[pathIdx];
+        next.x = Field.ToWorldX(v.X);
+        next.y = Field.ToWorldY(v.Y);
+        //パス番号を進める 
+        pathIdx++;
+    }
+}
---
+++ DefenceWitches/Assets/Scripts/Field.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// フィールド情報の管理
+public class Field : Token
+{
+    //何もない
+    public const int CHIP_NONE = 0;
+    //開始地点 
+    public const int CHIP_PATH_START = 26;
+
+    //パス（座標リスト）
+    List<Vec2D> path;
+    public List<Vec2D> Path
+    {
+        get{ return path; }
+    }
+
+    // チップ1マスのサイズを取得する 
+    public static float GetChipSize()
+    {
+        var spr = GetChipSprite();
+        return spr.bounds.size.x;
+    }
+    
+    //チップサイズの基準となるスプライトを取得する
+    static Sprite GetChipSprite()
+    {
+        return Util.GetSprite("Levels/tileset","tileset_ 0");
+    } 
+
+    //チップ座標をワールドのX座標を取得する 
+    public static float ToWorldX(int i)
+    {
+        //カメラビューの左下の座標を取得
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        var spr = GetChipSprite();
+        var sprW = spr.bounds.size.x;
+
+        return min.x + (sprW * i) + sprW/2;
+    } 
+    
+    //チップ座標をワールドのY座標を取得する
+    public static float ToWorldY(int j)
+    { 
+        //カメラビューの右上の座標を取得する 
+        Vector2 max = Camera.main.ViewportToWorldPoint( new Vector2(1, 1));
+        var spr = GetChipSprite(); 
+        var sprH = spr.bounds.size.y;
+
+        //上下逆になるので逆さにして変換 
+        return max.y - (sprH * j) - sprH/2;
+    }
+
+    void Start()
+    {
+        // マップ読み込み 
+        TMXLoader tmx = new TMXLoader();
+        tmx.Load("Levels/map");
+
+        // 経路レイヤーを取得 
+        Layer2D lPath = tmx.GetLayer("path");
+
+        // 開始地点を検索 
+        Vec2D pos = lPath.Search(CHIP_PATH_START);
+
+        // 座標リストを作成 
+        path = new List <Vec2D>();
+        //　開始座標を座標リストに登録 
+        path.Add( new Vec2D(pos.X,pos.Y));
+        // 通路をふさぐ 
+        lPath.Set( pos.X, pos.Y, CHIP_NONE);
+        // 座標リスト作成 
+        CreatePath(lPath,pos.X,pos.Y, path); 
+    
+        // 敵を取得 
+        Enemy enemy = GameObject.Find("Enemy").GetComponent<Enemy>();
+
+        // パスを変換して敵を移動
+        enemy.Init( path);         
+    }
+
+    //パスを作る
+    void CreatePath( Layer2D layer, int x, int y, List < Vec2D > path){
+        // 左・上・右・下を調べる 
+        int[] xTbl = {-1, 0, 1, 0};
+        int[] yTbl = {0, -1, 0, 1};
+        for ( var i = 0; i < xTbl. Length; i ++)
+        {
+            int x2 = x + xTbl[ i];
+            int y2 = y + yTbl[ i];
+            int val = layer. Get( x2, y2); 
+            if( val > CHIP_NONE)
+            {
+                // 経路を発見 
+                // 経路をふさぐ 
+                layer. Set( x2, y2, CHIP_NONE);
+                // 座標を追加 
+                path.Add( new Vec2D( x2, y2));
+                // パス生成を再帰呼び出し 
+                CreatePath( layer, x2, y2, path);
+            }
+        }
+    }   
+}
