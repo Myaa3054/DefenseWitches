c2909607ee6ab5b7461a964614c3a87499863837
xsay86 <take6586@gmail.com>
2019-10-25 01:22:02 +0900

    とりあえず簡易なタイトルを作成しました。

---
+++ DefenceWitches/Assets/Scripts/Title.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Title : MonoBehaviour
+{
+    private int scenestate = 0;
+    public int maxpage = 2;
+    public GameObject illust; //ここにプレイ方法の書いたオブジェクトを入れる
+    // Start is called before the first frame update
+   
+    public void OnClick()
+    {
+        scenestate++;
+       if (scenestate == 1)
+        {
+            illust.SetActive(true);
+        }
+        if (scenestate >= maxpage) 
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+    // Update is called once per frame
+    
+}
+
